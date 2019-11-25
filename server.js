34240f02c9172b16618ac7d5b8b0b73d3391ca75
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-11-25 02:03:51 +0100

    fix: cart

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // allows our project read variables from .env files
-require("dotenv").config();  
+require("dotenv").config();
 
 const express = require("express");
 const sequelize = require("./config/database");
 const path = require("path");
+const morgan = require("morgan")
 
 // Enables Cross Origin Resource Sharing for our Project
 const cors = require("cors");
@@ -21,6 +22,7 @@ const app = express();
 // its contents via 'req.body' object
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"))
 
 
 // Create a static directory for our uploads
@@ -33,9 +35,28 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/order", orderRoutes);
 app.use("/api/v1/cart", cartRoutes);
 
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV != "development") {
+        return next(err);
+    }
+
+    return res.status(err.statusCode || 500).json({
+        status: "error",
+        message: err.message || "An unexpected error occurred",
+        stack: err.stack || "stack trace"
+    });
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        status: "error",
+        message: err.message || "An unexpected error occurred"
+    });
+})
+
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync() 
+sequelize.sync()
     .then(result => {
     app.listen(PORT, () => console.log("Server is running on port 5000"))
 })
